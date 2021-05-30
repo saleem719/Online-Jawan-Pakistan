36dddd132b9fb9cfa423cf5d7c77a95329fde4fb
saleem baig <saleem.duhs@gmail.com>
2021-05-30 17:30:07 +0500

    work complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,8 +10,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (
-        Scaffold(body: Text("My name is Saleem"))
-        ));
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Welcome ! Please Login'),
+          ),
+        ),
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage('images/profile.jpg'),
+                ),
+                Text(
+                  "Saleem baig",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Flutter Developer",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Please enter email'),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: 200,
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: 'Please enter password'),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("login"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
